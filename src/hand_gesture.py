07c97d6253e3057fdb0aea0448d5c06d0ccd68de
Mukund Gupta <73193030+mukundgupta-1@users.noreply.github.com>
2022-01-09 20:27:32 +0530

    Updated Lib Cvzone V1.5.4 & mediapipe V0.8.9.1

--- src/hand_gesture.py
+++ src/hand_gesture.py
@@ -1,17 +1,25 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
-from cvzone.SerialModule import SerialObject
-import cvzone
+
 cap = cv2.VideoCapture(0)
-detector = HandDetector(mode=False,maxHands=1,detectionCon=1)
-mySerial = cvzone.SerialObject("COM9",9600,1)
-while True
-  success,img = cap.read()
-  img = detector.findHands(img)
-  lmlist,bbox = detector.findPosition(img)
-  if lmlist:
-      fingers = detector.fingersUp()
-      #print(fingers)
-      mySerial.sendData(fingers)
-  cv2.imshow("Image",img)
-  cv2.waitKey(1) 
+detector = HandDetector(detectionCon=0.8,maxHands=1)
+
+while True:
+    success, img = cap.read()
+    hands, img = detector.findHands(img)
+
+    if hands:
+        hand1 = hands[0]
+        lmList1 = hand1["lmList"]
+        bbox1 = hand1["bbox"]
+        centerPoint1 = hand1["center"]
+        handType1 = hand1["type"]
+
+        #print(len(lmList1),lmList1)
+        #print(bbox1)
+        #print(centerPoint1)
+        fingers1 = detector.fingersUp(hand1)
+        print(fingers1)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
