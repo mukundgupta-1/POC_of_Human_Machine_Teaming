576a9bc5318e21e29206360ea4f23d105356b8b1
Mukund Gupta <73193030+mukundgupta-1@users.noreply.github.com>
2022-01-11 22:51:30 +0530

    Addition of serial library to send data

--- src/hand_gesture.py
+++ src/hand_gesture.py
@@ -1,8 +1,10 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+from cvzone.SerialModule import SerialObject
+arduino = SerialObject("COM9")
 
 cap = cv2.VideoCapture(0)
-detector = HandDetector(detectionCon=0.8,maxHands=1)
+detector = HandDetector(detectionCon=0.8, maxHands=2)
 
 while True:
     success, img = cap.read()
@@ -14,12 +16,21 @@ while True:
         bbox1 = hand1["bbox"]
         centerPoint1 = hand1["center"]
         handType1 = hand1["type"]
-
-        #print(len(lmList1),lmList1)
-        #print(bbox1)
-        #print(centerPoint1)
         fingers1 = detector.fingersUp(hand1)
-        print(fingers1)
+        if len(hands) == 2:
+            hand2 = hands[1]
+            lmList2 = hand2["lmList"]
+            bbox2 = hand2["bbox"]
+            centerPoint2 = hand2["center"]
+            handType2 = hand2["type"]
 
+            fingers2 = detector.fingersUp(hand2)
+            print(fingers1, fingers2)
+        else:
+            # print(len(lmList1),lmList1)
+            # print(bbox1)
+            # print(centerPoint1)
+            print(fingers1)
+            arduino.sendData(fingers1)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
