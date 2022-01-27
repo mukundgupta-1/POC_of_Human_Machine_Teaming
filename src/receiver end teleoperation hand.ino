1189d5a6788006425be667b57a12d9774e4c8850
Mukund Gupta <73193030+mukundgupta-1@users.noreply.github.com>
2022-01-27 18:57:02 +0530

    Loop execution changed for print

--- src/receiver end teleoperation hand.ino
+++ src/receiver end teleoperation hand.ino
@@ -10,9 +10,9 @@ Servo servoMiddle;
 Servo servoRing;
 Servo servoPinky;
 
-RF24 radio(5, 6);
-const byte address[6] = "00001"; //either add radio address here or in pipe
-int msg[5];
+RF24 radio(9, 8);
+const byte address [6] = "00001"; //either add radio address here or in pipe
+
 //const uint64_t pipe = 0xE8E8F0F0E1LL;
 
 struct Data_Package {
@@ -21,40 +21,32 @@ byte servoIndex;
 byte servoMiddle;
 byte servoRing;
 byte servoPinky;
-}
+};
 
-void setup() {
-  
+void setup() { 
 Serial.begin(19200);
-servoThumb.attach(7);
-servoIndex.attach(8);
-servoMiddle.attach(9);
-servoRing.attach(10);
-servoPinky.attach(11);
+servoThumb.attach(1);
+servoIndex.attach(2);
+servoMiddle.attach(3);
+servoRing.attach(4);
+servoPinky.attach(5);
 
 radio.begin();
 radio.openReadingPipe(1, address);
 radio.startListening();
 }
-
-void  receieveData(){
-
-// data recived through nrf 
-
-}
-
 void loop() {
-  if(radio.available()){
-    //receieveData();
-    bool done = false;
-    while (!done){
-      done = radio.read(valsRec, sizeof(valsRec));
-    
+  while(radio.available()){
+    Serial.println("data come ");
+    int valsRec[5];
+    for (int i = 0; i < 5; i = i + 1){    
+    radio.read(&valsRec, sizeof(valsRec));
+    Serial.print(valsRec[i]);
     if (valsRec[0]==1){servoThumb.write(180);}else{servoThumb.write(0);}
     if (valsRec[1]==1){servoIndex.write(180);}else{servoIndex.write(0);}
     if (valsRec[2]==1){servoMiddle.write(180);}else{servoMiddle.write(0);}
     if (valsRec[3]==1){servoRing.write(180);}else{servoRing.write(0);}
     if (valsRec[4]==1){servoPinky.write(180);}else{servoPinky.write(0);}
     }
-  }
-}
+      }
+        } 
