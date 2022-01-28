fa0bca0244461d83bc5b79f21d7597721066fc4d
Mukund Gupta <73193030+mukundgupta-1@users.noreply.github.com>
2022-01-28 06:04:38 +0530

    Addition of data transmission loop

--- src/receiver end teleoperation hand.ino
+++ src/receiver end teleoperation hand.ino
@@ -40,13 +40,13 @@ void loop() {
     Serial.println("data come ");
     int valsRec[5];
     for (int i = 0; i < 5; i = i + 1){    
-    radio.read(&valsRec, sizeof(valsRec));
-    Serial.print(valsRec[i]);
+    radio.read(&valsRec[i], sizeof(valsRec[i]));
+    Serial.print(valsRec[i]);}
     if (valsRec[0]==1){servoThumb.write(180);}else{servoThumb.write(0);}
     if (valsRec[1]==1){servoIndex.write(180);}else{servoIndex.write(0);}
     if (valsRec[2]==1){servoMiddle.write(180);}else{servoMiddle.write(0);}
     if (valsRec[3]==1){servoRing.write(180);}else{servoRing.write(0);}
     if (valsRec[4]==1){servoPinky.write(180);}else{servoPinky.write(0);}
-    }
+    
       }
         } 
